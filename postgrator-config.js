1bff3310d14aecc1a58d69fecf6c0df2bd35af65
Eduardo Gonzalez <eg9800@gmail.com>
2020-04-04 15:51:16 -0700

    added to config file

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,10 +4,12 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL,
-  "ssl": !!process.env.SSL,
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
   "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "password": process.env.MIGRATION_DB_PASS,
+  "ssl": !!process.env.SSL,
+  "listen_addresses" : '*'
 }
+
