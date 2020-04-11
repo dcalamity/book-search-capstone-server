2c0c68fc6cb876aeaee3e651577dceccea268864
Eduardo Gonzalez <eg9800@gmail.com>
2020-04-10 17:53:25 -0700

    fixing syntax

--- src/book_collections/book_collections-router.js
+++ src/book_collections/book_collections-router.js
@@ -59,16 +59,14 @@ BookCollectionsRouter
             req.params.user_id
         )
             .then(book_collection => {
-                
+                console.log(book_collection, 'book_collection')
                 if (!book_collection) {
                     return res.status(404).json({
                         error: { message: `user_id doesn't exist` }
                     })
                 }
-                console.log(book_collection, 'book_collection')
                 res.json(book_collection)
-                // res.book_collection = book_collection // save the book_collection for the next middleware
-                next() // don't forget to call next so the next middleware happens!
+                next() 
             })
             .catch(next)
     })
